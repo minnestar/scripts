d5f4b752a7cf04a52bb9163650923d2540bdcbe8
Jamie Thingelstad <jamie@thingelstad.com>
2012-04-05 07:35:24 -0500

    Updated to handle additional fields in the JSON output from session.

--- etl/session-json-to-csv.py
+++ etl/session-json-to-csv.py
@@ -12,15 +12,37 @@ import json
 import csv
 import urllib2
 import commands
+import time
+from datetime import datetime
+from datetime import timedelta
+from pytz import timezone
 
-req="http://sessions.minnestar.org/events/2.json"
+def bool2str(v):
+    if v:
+        return "Yes"
+    else:
+        return "No"
+
+req="http://sessions.minnestar.org/events/3.json"
 data= urllib2.urlopen(req).read()
 x=json.loads(data)
 
 f=csv.writer(open("output.csv","wb+"))
 
 # Write CSV Header, If you dont need that, remove this line
-f.writerow(["Title", "Session[Event]", "Session[Presenter]", "Free Text"])
+f.writerow(["Title",
+	"Session[Event]",
+        "Session[Summary]"
+	"Session[Presenter]",
+	"Session[Projector]",
+	"Session[Panel]",
+        "Session[Room]",
+	"Session[Start hour]",
+	"Session[Start minute]",
+	"Session[End hour]",
+	"Session[End minute]",
+	"Free Text"])
+
 out_event = x["event"]["name"]
 
 for x in x["event"]["sessions"]:
@@ -40,9 +62,23 @@ for x in x["event"]["sessions"]:
     # Use pandoc to convert markdown to mediawiki
     out_description = commands.getoutput('/usr/local/bin/pandoc -f markdown -t mediawiki temp.md')
     
-    out_presenter = x["participant"]["name"].encode('ascii', 'ignore') 
-    
+    out_presenter = ', '.join(x["presenter_names"]).encode('ascii', 'ignore') 
+    out_summary = x["summary"].encode('ascii', 'ignore')
+    out_panel = bool2str(x["panel"])
+    out_projector = bool2str(x["projector"])
+    out_room = x["room_name"]
+
+    out_time = x["starts_at"]
+    start_time = datetime.strptime(out_time, "%Y-%m-%dT%H:%M:%S-05:00")
+    start_time = start_time.replace(tzinfo=timezone('US/Central'))
+    out_start_hour = start_time.strftime("%I")
+    out_start_minute = start_time.strftime("%M")
+    end_time = start_time + timedelta(minutes=45)
+    out_end_hour = end_time.strftime("%I")
+    out_end_minute = end_time.strftime("%M")
+ 
     print "Writing " + out_title
-    f.writerow([out_title, out_event, out_presenter, out_description])
+    f.writerow([out_title, out_event, out_summary, out_presenter, out_projector, out_panel, 
+        out_room, out_start_hour, out_start_minute, out_end_hour, out_end_minute, out_description])
 
 	
